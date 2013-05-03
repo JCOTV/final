0767b03a96cb8d8b1090b6bfad925a5c2e0ab376
Jose Carlos Onofre <jonofref@gmail.com>
2013-05-03 10:21:03 -0500

    Primera
    Este es un ejemplo para usar Github y Phonegap Build

---
+++ gen/R.java.d
@@ -0,0 +1,14 @@
+C:\Users\odetita\RippleSites\Android\templates\final\gen\com\cfemex\lv\mobile\gasolinera\R.java \
+ : C:\Users\odetita\RippleSites\Android\templates\final\res\drawable\icon.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-hdpi\icon.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-hdpi\ic_launcher.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-ldpi\icon.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-ldpi\ic_launcher.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-mdpi\icon.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-mdpi\ic_launcher.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-xhdpi\icon.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\drawable-xhdpi\ic_launcher.png \
+C:\Users\odetita\RippleSites\Android\templates\final\res\layout\main.xml \
+C:\Users\odetita\RippleSites\Android\templates\final\res\values\strings.xml \
+C:\Users\odetita\RippleSites\Android\templates\final\res\xml\config.xml \
+C:\Users\odetita\RippleSites\Android\templates\final\bin\AndroidManifest.xml \
